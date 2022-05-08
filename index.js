6d543f75fc9b2c2c0abaf06bc91f78a90138ddd2
trinhxhai <trinhxhai@gmail.com>
2022-05-08 22:23:34 +0700

    fix fixed port 2

--- index.js
+++ index.js
@@ -97,5 +97,5 @@ socketIo.on("connection", (socket) => {
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${port}`);
 });
